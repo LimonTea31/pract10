aa8abc34c662731ef3d3b8db2e35f0bb90525d8d
Alyona <LoverTea31@gmail.com>
2023-06-02 10:11:14 +0700

    Git

---
+++ app/src/main/java/com/example/pract10/ShowItem.java
@@ -0,0 +1,13 @@
+package com.example.pract10;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ShowItem extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_item);
+    }
+}
